1c08e37d5a442c8f4ab95312786aa70242393c2d
Mateus Huebra <mateus.huebra@scandiweb.com>
2025-02-05 21:33:50 -0300

    added coverage to PersonController

---
+++ tests/Feature/PersonTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Person;
+
+class PersonTest extends TestCase
+{
+    //use RefreshDatabase;
+    public function setUp(): void {
+        parent::setUp();
+        $user = User::factory()->create();
+        $this->actingAs($user);
+    }
+
+    public function test_person_can_be_created() {
+        $data = [
+            'first_name' => 'Mateus',
+            'last_name' => 'Huebra',
+            'gender' => 'male',
+            'birthday' => '2001-04-13',
+            'cpf' => '12345678909',
+        ];
+
+        $response = $this->post(route('people.store'), $data);
+
+        $response->assertRedirect(route('people'));
+        $this->assertDatabaseHas('people', $data);
+    }
+
+    public function test_person_cant_be_created_with_invalid_date() {
+        $data = [
+            'first_name' => 'Teste',
+            'last_name' => '',
+            'gender' => 'male',
+            'birthday' => '3001-04-13',
+            'cpf' => '12345678909',
+        ];
+
+        $response = $this->post(route('people.store'), $data);
+
+        $response->assertInvalid([
+            'birthday' => 'The birthday field must be a date before today.'
+        ]);
+        $this->assertDatabaseMissing('people', $data);
+    }
+}
